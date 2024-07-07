8a77c4a6ab22bc6b30666819b02549d58d8f9f32
Guilherme Neis <guilhermeneis132@gmail.com>
2024-07-07 15:48:09 -0300

    feat: verificações basicas de texto no comando timer

--- cli.py
+++ cli.py
@@ -2,36 +2,61 @@ import time
 import threading
 import curses
 
+
 def timer_print(minutos, segundos):
     x = 0
     y = 0
     for _minutos in reversed(range(0, minutos)):
         global global_minutos
-        global_minutos = _minutos+1 
+        global_minutos = _minutos + 1
         for _seg in reversed(range(0, segundos)):
             global global_segundos
-            global_segundos = _seg+1
+            global_segundos = _seg + 1
             if should_global_thread_run is False:
                 return
             yx = curses.getsyx()
             x = yx[1]
             y = yx[0]
-            screen.addstr(0,0, f"{_minutos}:{_seg}")
-            screen.move(y,x)
+            screen.addstr(0, 0, f"{_minutos}:{_seg}")
+            screen.move(y, x)
             screen.refresh()
             time.sleep(1)
-    screen.addstr(y,x, "O tempo acabou!")
+    screen.addstr(y, x, "O tempo acabou!")
     screen.refresh()
-    
-def createClockThread(minutos, segundos = 60):
-    return threading.Thread(target = timer_print, args=[minutos, segundos], daemon=True)
+
+
+def print_on_screen(text):
+    global last_x
+    screen.addstr(last_x, 0, text)
+    last_x = last_x + 1
+
+
+def print_unknown_command(command):
+    print_on_screen(f"Comando desconhecido! {command}")
+
+
+def print_missing_args(command):
+    print_on_screen(f"Faltam argumentos para o comando {command}!")
+
+
+def create_clock_thread(minutos, segundos=60):
+    return threading.Thread(target=timer_print, args=[minutos, segundos], daemon=True)
+
+
+def run_clock_thread(minutos, segundos=60):
+    global global_clock_thread
+    global_clock_thread = create_clock_thread(minutos, segundos)
+    global should_global_thread_run
+    should_global_thread_run = True
+    global_clock_thread.start()
+
 
 # def handle_resize():
 #    while True:
 #        com = screen.getch()
 #        if com == curses.KEY_RESIZE:
-#            screen.resize() 
-      
+#            screen.resize()
+
 last_y = 0
 last_x = 1
 tasks = []
@@ -46,57 +71,78 @@ screen.clear()
 while True:
     while True:
         screen.refresh()
-        screen.addstr(last_x,0,">>")
+        screen.addstr(last_x, 0, ">>")
         screen.clrtoeol()
-        inputted_value = screen.getstr(last_x,3).decode('utf-8').strip()
+        inputted_value = screen.getstr(last_x, 3).decode("utf-8").strip()
+        last_x = last_x + 1
         last_y = 3
-        last_x = last_x+1
         comandos = inputted_value.split()
-        if comandos[0]=="timer":
+        input_number = len(comandos)
+        if input_number == 0:
+            continue
+        if comandos[0] == "timer":
+            if input_number == 1:
+                continue
+            # testar se não precisa de um try catch aqui
             tempo = int(comandos[1])
-            if comandos[2]=="tasks":
-                for _ in comandos[3].split(","):
-                    tasks.append(_)
-            global_clock_thread = createClockThread(tempo)
-            should_global_thread_run = True
-            global_clock_thread.start()
-        elif comandos[0]=="tasks":
+            if input_number == 2:
+                run_clock_thread(tempo)
+                continue
+            if comandos[2] != "+":
+                print_unknown_command(comandos[2])
+                continue
+            if input_number == 3:
+                print_missing_args(comandos[2])
+                continue
+            if comandos[3] == "tasks":
+                # checar se o for em uma lista vazia da exception
+                if input_number == 4:
+                    print_missing_args(comandos[3])
+                    continue
+                for _ in comandos[4].split(","):
+                    tasks.append(_.strip())
+                run_clock_thread(tempo)
+            else:
+                print_unknown_command(comandos[3])
+                continue
+        elif comandos[0] == "tasks":
             for index, _ in enumerate(tasks):
-                screen.addstr(last_x, 0, f"Tarefa {index+1} - {_}")
-                last_x = last_x+1
-        elif comandos[0]=="check":
+                screen.addstr(last_x, 0, f"Tarefa {index + 1} - {_}")
+                last_x = last_x + 1
+        elif comandos[0] == "check":
             for _ in comandos[1].split(","):
                 if _ in tasks:
                     tasks.remove(_)
                 screen.addstr(last_x, 0, f"Tarefa {_} concluída!")
-                last_x = last_x+1
-        elif comandos[0]=="remove":
+                last_x = last_x + 1
+        elif comandos[0] == "remove":
             for _ in comandos[1].split(","):
                 tasks.remove(_)
                 screen.addstr(last_x, 0, f"Tarefa {_} removida!")
-                last_x = last_x+1
-        elif comandos[0]=="uncheck":
+                last_x = last_x + 1
+        elif comandos[0] == "uncheck":
             for _ in comandos[1].split(","):
                 tasks.add(_)
-                last_x = last_x+1
-        elif comandos[0]=="edit":
+                last_x = last_x + 1
+        elif comandos[0] == "edit":
             for _ in comandos[1].split(","):
                 indice = tasks.index(_)
                 valorAntigo = tasks[indice]
                 valorNovo = comandos[3]
                 tasks[indice] = valorNovo
                 screen.addstr(last_x, 0, f"Tarefa {valorAntigo} agora é {valorNovo}!")
-                last_x = last_x+1
-        elif comandos[0]=="stop":
+                last_x = last_x + 1
+        elif comandos[0] == "stop":
             should_global_thread_run = False
             global_clock_thread = None
-        elif(comandos[0] == "pause"):
+        elif comandos[0] == "pause":
             should_global_thread_run = False
-        elif(comandos[0]=="continue"):
-            if(global_clock_thread is not None):
+        elif comandos[0] == "continue":
+            if global_clock_thread is not None:
                 should_global_thread_run = True
-                global_clock_thread = createClockThread(global_minutos, global_segundos)
+                global_clock_thread = create_clock_thread(
+                    global_minutos, global_segundos
+                )
                 global_clock_thread.start()
         else:
-            next
-    
+            continue
