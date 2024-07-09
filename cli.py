b952958a57024da9225deb85878a7718203a286e
Guilherme Neis <guilhermeneis132@gmail.com>
2024-07-08 23:02:05 -0300

    Feat "função para finalizar o timer"

--- cli.py
+++ cli.py
@@ -81,85 +81,86 @@ curses.curs_set(1)
 screen.keypad(True)
 screen.clear()
 while True:
-    while True:
-        screen.refresh()
-        screen.addstr(last_x, 0, ">>")
-        screen.clrtoeol()
-        inputted_value = screen.getstr(last_x, 3).decode("utf-8").strip()
-        last_x = last_x + 1
-        last_y = 3
-        comandos = split_not_empty(inputted_value)
-        input_number = len(comandos)
-        if input_number == 0:
+    screen.refresh()
+    screen.addstr(last_x, 0, ">>")
+    screen.clrtoeol()
+    inputted_value = screen.getstr(last_x, 3).decode("utf-8").strip()
+    last_x = last_x + 1
+    last_y = 3
+    comandos = split_not_empty(inputted_value)
+    input_number = len(comandos)
+    if input_number == 0:
+        continue
+    if comandos[0] == "timer":
+        if input_number == 1:
+            print_missing_args(comandos[0])
             continue
-        if comandos[0] == "timer":
-            if input_number == 1:
-                print_missing_args(comandos[0])
+        # testar se não precisa de um try catch aqui
+        tempo = int(comandos[1])
+        if input_number == 2:
+            run_clock_thread(tempo)
+            continue
+        if comandos[2] != "+":
+            print_unknown_command(comandos[2])
+            continue
+        if input_number == 3:
+            print_missing_args(comandos[2])
+            continue
+        if comandos[3] == "tasks":
+            raw_args = inputted_value.split("tasks")[1]
+            if is_empty(raw_args):
+                print_missing_args(comandos[3])
                 continue
-            # testar se não precisa de um try catch aqui
-            tempo = int(comandos[1])
-            if input_number == 2:
-                run_clock_thread(tempo)
+            task_args = split_not_empty(raw_args, ",")
+            if len(task_args) == 0:
                 continue
-            if comandos[2] != "+":
-                print_unknown_command(comandos[2])
-                continue
-            if input_number == 3:
-                print_missing_args(comandos[2])
-                continue
-            if comandos[3] == "tasks":
-                raw_args = inputted_value.split("tasks")[1]
-                if is_empty(raw_args):
-                    print_missing_args(comandos[3])
-                    continue
-                task_args = split_not_empty(raw_args, ",")
-                if len(task_args) == 0:
-                    continue
-                # checar se o for em uma lista vazia da exception
-                for _ in task_args:
-                    tasks.append(_.strip())
-                run_clock_thread(tempo)
-            else:
-                print_unknown_command(comandos[3])
-                continue
-        elif comandos[0] == "tasks":
-            for index, _ in enumerate(tasks):
-                screen.addstr(last_x, 0, f"Tarefa {index + 1} - {_}")
-                last_x = last_x + 1
-        elif comandos[0] == "check":
-            for _ in comandos[1].split(","):
-                if _ in tasks:
-                    tasks.remove(_)
-                screen.addstr(last_x, 0, f"Tarefa {_} concluída!")
-                last_x = last_x + 1
-        elif comandos[0] == "remove":
-            for _ in comandos[1].split(","):
-                tasks.remove(_)
-                screen.addstr(last_x, 0, f"Tarefa {_} removida!")
-                last_x = last_x + 1
-        elif comandos[0] == "uncheck":
-            for _ in comandos[1].split(","):
-                tasks.add(_)
-                last_x = last_x + 1
-        elif comandos[0] == "edit":
-            for _ in comandos[1].split(","):
-                indice = tasks.index(_)
-                valorAntigo = tasks[indice]
-                valorNovo = comandos[3]
-                tasks[indice] = valorNovo
-                screen.addstr(last_x, 0, f"Tarefa {valorAntigo} agora é {valorNovo}!")
-                last_x = last_x + 1
-        elif comandos[0] == "stop":
-            should_global_thread_run = False
-            global_clock_thread = None
-        elif comandos[0] == "pause":
-            should_global_thread_run = False
-        elif comandos[0] == "continue":
-            if global_clock_thread is not None:
-                should_global_thread_run = True
-                global_clock_thread = create_clock_thread(
-                    global_minutos, global_segundos
-                )
-                global_clock_thread.start()
+            # checar se o for em uma lista vazia da exception
+            for _ in task_args:
+                tasks.append(_.strip())
+            run_clock_thread(tempo)
         else:
+            print_unknown_command(comandos[3])
             continue
+    elif comandos[0] == "tasks":
+        for index, _ in enumerate(tasks):
+            screen.addstr(last_x, 0, f"Tarefa {index + 1} - {_}")
+            last_x = last_x + 1
+    elif comandos[0] == "check":
+        for _ in comandos[1].split(","):
+            if _ in tasks:
+                tasks.remove(_)
+            screen.addstr(last_x, 0, f"Tarefa {_} concluída!")
+            last_x = last_x + 1
+    elif comandos[0] == "remove":
+        for _ in comandos[1].split(","):
+            tasks.remove(_)
+            screen.addstr(last_x, 0, f"Tarefa {_} removida!")
+            last_x = last_x + 1
+    elif comandos[0] == "uncheck":
+        for _ in comandos[1].split(","):
+            tasks.add(_)
+            last_x = last_x + 1
+    elif comandos[0] == "edit":
+        for _ in comandos[1].split(","):
+            indice = tasks.index(_)
+            valorAntigo = tasks[indice]
+            valorNovo = comandos[3]
+            tasks[indice] = valorNovo
+            screen.addstr(last_x, 0, f"Tarefa {valorAntigo} agora é {valorNovo}!")
+            last_x = last_x + 1
+    elif comandos[0] == "stop":
+        should_global_thread_run = False
+        global_clock_thread = None
+    elif comandos[0] == "pause":
+        should_global_thread_run = False
+    elif comandos[0] == "continue":
+        if global_clock_thread is not None:
+            should_global_thread_run = True
+            global_clock_thread = create_clock_thread(
+                global_minutos, global_segundos
+            )
+            global_clock_thread.start()
+    elif comandos[0] == "quit":
+        break
+    else:
+        continue
